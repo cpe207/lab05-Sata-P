1fc1253c1a84557b2b77efb26a8694cf976e6263
Sata-P <programhaha@gmail.com>
2024-07-06 19:21:31 +0700

    done!

--- q1.ts
+++ q1.ts
@@ -1,29 +1,40 @@
 // define interface for Hero and Shop objects
-interface Hero {
-  /* Your code here */
-}
 
-interface Shop {
-  /* Your code here */
+//660612158 ศตคุณ นนทา
+
+interface Hero {
+  items: string[];
+  gold: number;
+
+}
+interface Shop{
+  item: string;
+  price: number;
 }
 
 // assign interface/type to the function definition properly
-function buyItem(hero, shop) {
-  /* Your code here */
+function buyItem(hero:Hero, shop:Shop){
+  
+  if (hero.gold >= shop.price){
+    hero.gold -= shop.price;
+    hero.items.push(shop.item);
+  }
+  return hero;
 }
 
-//Test cases : assign proper type/interface to all objects
+
+
 const hero1 = {
   items: ["sword", "potion"],
   gold: 50,
 };
 
-const shop1 = {
+const shop1= {
   item: "armor",
   price: 20,
 };
 
-const hero2 = {
+const hero2= {
   items: ["sword", "potion"],
   gold: 50,
 };
--- q2.ts
+++ q2.ts
@@ -1,13 +1,22 @@
-// define interface for Student object
-/* Your code here */
+interface Student{
 
-// assign interface/type to the function definition properly
-function findTopNames(students) {
-  /* Your code here */
+  name:string;
+  score:number;
+
+}
+
+
+function findTopNames(students:Student[]):string[] {
+  
+  const filtered = students
+   .filter(student => student.score >= 9)
+   .map(student => student.name);
+   return filtered;
+   
 }
 
 // assign interface/type to the student1 object properly
-const students1 = [
+const students1: Student[] = [
   { name: "john", score: 10 },
   { name: "jane", score: 9 },
   { name: "jim", score: 8 },
